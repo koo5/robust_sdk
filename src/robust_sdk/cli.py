3fcb6c4ef3f0763d8c39489bb2f160554ddbf35b
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-06-27 20:35:04 +0200

    ..!

--- src/robust_sdk/cli.py
+++ src/robust_sdk/cli.py
@@ -1,16 +1,24 @@
 import click
 
+# https://docs.python.org/3/library/xml.etree.elementtree.html
+import xml.etree.ElementTree as xmltree
 
 
-@click.command()
+
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+def cli(debug):
+    click.echo(f"Debug mode is {'on' if debug else 'off'}")
+
+@cli.command()  # @cli, not @click!
 @click.option('--xml', help='Legacy ROBUST IC XML file.')
+def xml2rdf(xml):
+	"""Load the XML IC file and produce robust IC RDF"""
+	x = xmltree.parse(xml).getroot()
+	print(x)
 
-def xml2rdf(count, name):
-	"""Simple program that greets NAME for a total of COUNT times."""
-	for x in range(count):
-		click.echo(f"Hello {name}!")
 
 if __name__ == '__main__':
-	hello()
+	cli()
 	
 	
