871ff20bb8bb346603de267fd4fa1e2a287cf301
Jindrich Kolman <kolman.jindrich@gmail.com>
2023-08-03 16:20:33 +0200

    fixes

--- src/robust_sdk/utils.py
+++ src/robust_sdk/utils.py
@@ -3,20 +3,8 @@ from datetime import datetime
 from rdflib.collection import Collection
 from rdflib import Graph, RDF
 from rdflib.term import Literal, BNode, Identifier
+from .prefixes import *
 
-
-def AssertValue(g: Graph, value: any):
-	v = BNode()
-	g.add((v, RDF.value, value))
-	return v
-
-def AssertLiteralValue(g: Graph, value: any):
-	v = BNode()
-	g.add((v, RDF.value, Literal(value)))
-	return v
-
-def AssertListValue(g: Graph, items: list[Identifier]):
-	return AssertValue(g, AssertList(g, items))
 
 
 def AssertList(g, seq: list[Identifier]):
